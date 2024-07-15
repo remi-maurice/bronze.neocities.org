04c01cf9aa391d0225ebbc70a0e4377c2329e1ed
freakonaut <bouliaczoo@gmail.com>
2024-07-15 11:51:00 +0200

    maj du hook upload

--- upload.sh
+++ upload.sh
@@ -2,7 +2,7 @@
 # Hook to easily git push and neocities push
 
 git add .
-read -p "Enter commit:" commit_message
+read -p "Enter commit message:" commit_message
 git commit -m "$commit_message" 
 git push -u origin master
 read -p "Upload to neocities? (y/n): " diff_confirm
@@ -11,3 +11,11 @@ if [ "$diff_confirm" != "y" ]; then
     exit 1
 fi
 neocities push website
+echo "Upload finished!"
+echo "Press 'q' to quit."
+while true; do
+    read -n 1 -r key
+    if [ "$key" = 'q' ]; then
+        break
+    fi
+done
