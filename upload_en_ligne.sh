4a60a0964295399b333681ef35ca194d582a84c0
freakonaut <bouliaczoo@gmail.com>
2024-09-02 14:10:45 +0200

    a

--- upload_en_ligne.sh
+++ upload_en_ligne.sh
@@ -8,7 +8,6 @@ read -p "Message pour l'historique( et appui sur entré):" commit_message
 git commit -m "$commit_message" 
 git push -u origin master
 
-
 if [ -f "$HOME/.bashrc" ]; then
     source "$HOME/.bashrc"
 else
@@ -18,6 +17,7 @@ fi
 #Push vers neo
 start_time=$(date +%s)
 $HOME/.local/share/gem/ruby/3.0.0/bin/neocities push --prune website
+
 # Fin du chronomètre et calcul du temps écoulé
 end_time=$(date +%s)
 elapsed_time=$((end_time - start_time))
