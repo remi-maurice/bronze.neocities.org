926d26312281ea1d387e89e2ab25b70c24a1d8e8
freakonaut <bouliaczoo@gmail.com>
2025-01-07 14:59:56 +0100

    test

--- upload_tout.sh
+++ upload_tout.sh
@@ -74,7 +74,7 @@ generate_image_list() {
 
         # Construire la description
 
-        description="$price $dimensions $weight"
+        description="$price € $dimensions cm $weight gr"
 
         # Écrire dans le fichier YAML
         echo "  - src: img/gallerie/${base_name}.webp" >> $OUTPUT_FILE
